296fde025e2d57a8ad98dcd71ca8c214e877d125
Jo-HyeRin <sayhyelin12@naver.com>
2023-03-03 16:45:07 +0900

    initial commit

---
+++ src/main/java/com/tistory/ppoddorin/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.ppoddorin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
